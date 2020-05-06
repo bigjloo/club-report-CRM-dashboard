ef3766ac021f07ae8596b55b00c11e2981030479
junzhong loo <junzhongloo@gmail.com>
2020-05-06 18:09:16 +0800

    finished models

--- poker_backend/agents/admin.py
+++ poker_backend/agents/admin.py
@@ -2,6 +2,20 @@ from django.contrib import admin
 from agents.models import Club, Agent, Account
 # Register your models here.
 
+"""
+class AccountInline(admin.StackedInline):
+    model = Account.agent.through
+    extra = 1
+
+
+class AgentAdmin(admin.ModelAdmin):
+    inlines = [AccountInline]
+
+
+class AccountAdmin(admin.ModelAdmin):
+    filter_horizontal = ("agent",)
+"""
+
 admin.site.register(Club)
 admin.site.register(Agent)
 admin.site.register(Account)
--- poker_backend/agents/models.py
+++ poker_backend/agents/models.py
@@ -4,37 +4,39 @@ from django.contrib.auth.models import User
 # Create your models here.
 
 
+class Club(models.Model):
+    name = models.CharField(max_length=64)
+    code = models.CharField(max_length=3)
+    platforms = (
+        ('UP', 'Upoker'),
+        ('PB', 'PokerBros'),
+        ('PPP', 'PPPoker'),
+    )
+    chip_value = models.DecimalField(max_digits=3, decimal_places=2)
+    platform = models.CharField(max_length=3, choices=platforms, default='PB')
+
+    def __str__(self):
+        return f"{self.name} ({self.code})"
+
+
 class Agent(models.Model):
     nickname = models.CharField(max_length=64)
     club_agent_id = models.IntegerField()
     club = models.ForeignKey(
         Club, on_delete=models.CASCADE, related_name="agents")
-    rakeback = models.DecimalField()
+    rakeback = models.DecimalField(max_digits=3, decimal_places=3)
     user = models.ForeignKey(
         User, on_delete=models.CASCADE, related_name="agents")
 
     def __str__(self):
-        return f"{self.name} - {self.club} - {self.rakeback}"
-
-
-class Club(models.Model):
-    name = models.CharField(max_length=64)
-    code = models.CharField(max_length=3)
-    platform = [
-        ('UP', 'Upoker'),
-        ('PB', 'PokerBros'),
-        ('PPP', 'PPPoker'),
-    ]
-    chip_value = models.DecimalField()
-
-    def __str__(self):
-        return f"{self.name} ({self.code})"
+        return f"{self.nickname} - {self.club} - {self.rakeback}"
 
 
 class Account(models.Model):
     nickname = models.CharField(max_length=64)
     club_account_id = models.IntegerField()
     club = models.ManyToManyField(Club, blank=True, related_name="accounts")
+    agent = models.ManyToManyField(Agent, related_name="accounts")
 
     def __str__(self):
         return f"{self.club_account_id} - {self.nickname}"
